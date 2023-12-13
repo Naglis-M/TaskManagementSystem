b5b4b0ad95cd4e2ce02e55066e2838706f5bedd7
Naglis-M <naglis.m15@gmail.com>
2023-12-13 16:29:49 +0000

    Added user authentication
    each user can only see their own tasks table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
@@ -12,7 +13,8 @@ class TasksController < ApplicationController
 
   # GET /tasks/new
   def new
-    @task = Task.new
+   # @task = Task.new
+   @task = current_user.tasks.build
   end
 
   # GET /tasks/1/edit
@@ -21,7 +23,8 @@ class TasksController < ApplicationController
 
   # POST /tasks or /tasks.json
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     respond_to do |format|
       if @task.save
@@ -57,6 +60,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    redirect_to tasks_path, notice: "Not Authorized To Edit This Task" if @task.nil?
+  end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
@@ -65,6 +74,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :description, :due_date, :priority, :completed)
+      params.require(:task).permit(:title, :description, :due_date, :priority, :completed, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_12_153745) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_150737) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_12_153745) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
